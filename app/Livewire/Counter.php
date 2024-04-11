230998fdefe0334de735f28467b20cc484370e8d
sarthaksavvy <sarthak@bitfumes.com>
2024-04-11 09:44:10 +0530

    [main] 06 - lots of layout trick

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -2,15 +2,19 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Layout('layouts.livewire-counter')]
 class Counter extends Component
 {
     public $count = 1;
 
+    // #[Title('Counter Pageeeeeee')]
     public function render()
     {
-        return view('livewire.counter');
+        return view('livewire.counter')->title('Counter Pageeeeeee');
     }
 
     function increment()
