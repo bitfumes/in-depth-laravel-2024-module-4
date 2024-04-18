052a7d6daddb896f4cb7798eb0706a4ae609bf7d
sarthaksavvy <sarthak@bitfumes.com>
2024-04-18 09:44:47 +0530

    13 - dynamic form - validate attribute

--- app/Livewire/DynamicForm.php
+++ app/Livewire/DynamicForm.php
@@ -3,15 +3,21 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class DynamicForm extends Component
 {
+    #[Validate('required')]
     public $name = "";
+
+    #[Validate('required|unique:users,email')]
     public $email = "";
 
     function submit()
     {
+        $this->validate();
+
         User::create([
             'name' => $this->name,
             'email' => $this->email,
--- resources/views/livewire/dynamic-form.blade.php
+++ resources/views/livewire/dynamic-form.blade.php
@@ -1,18 +1,28 @@
 <div class="flex justify-center mt-10">
-    <div class="bg-white p-4 shadow-md w-1/3 text-center rounded-ee-md m-auto">
+    <div class="bg-white p-4 shadow-md w-1/2 text-center rounded-ee-md m-auto">
         <h1 class="text-3xl">Please fill the form</h1>
         <div class="mt-4">
-            <div class="flex justify-between">
+            <div class="flex justify-around">
                 <div>
                     <label for="name">Name</label>
                     <input type="text" id="name" class="w-44 py-2 px-4 border border-gray-300 rounded-lg"
                         wire:model="name" placeholder="Fill name" />
+                    <div class="text-red-700 text-sm">
+                        @error('name')
+                            {{ $message }}
+                        @enderror
+                    </div>
                 </div>
 
                 <div>
                     <label for="email">Email</label>
                     <input type="email" id="email" class="w-44 py-2 px-4 border border-gray-300 rounded-lg"
                         wire:model="email" placeholder="Fill email" />
+                    <div class="text-red-700 text-sm">
+                        @error('email')
+                            {{ $message }}
+                        @enderror
+                    </div>
                 </div>
             </div>
 
